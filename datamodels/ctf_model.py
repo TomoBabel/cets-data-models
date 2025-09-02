907d5464f5bdb0d49753e3b6fb7aac86362172aa
JorMaister <jorgejjs86@gmail.com>
2025-09-02 11:32:56 +0200

    Clean ctf_model and add common configDict to init

--- datamodels/ctf_model.py
+++ datamodels/ctf_model.py
@@ -1,42 +1,7 @@
 from __future__ import annotations
-from typing import Any, Optional
-from pydantic import BaseModel, ConfigDict, Field, RootModel
-
-
-metamodel_version = "None"
-version = "0.0.1"
-
-
-class ConfiguredBaseModel(BaseModel):
-    model_config = ConfigDict(
-        validate_assignment=True,
-        validate_default=True,
-        extra="forbid",
-        arbitrary_types_allowed=True,
-        use_enum_values=True,
-        strict=False,
-    )
-    pass
-
-
-class LinkMLMeta(RootModel):
-    root: dict[str, Any] = {}
-    model_config = ConfigDict(frozen=True)
-
-    def __getattr__(self, key: str):
-        return getattr(self.root, key)
-
-    def __getitem__(self, key: str):
-        return self.root[key]
-
-    def __setitem__(self, key: str, value):
-        self.root[key] = value
-
-    def __contains__(self, key: str) -> bool:
-        return key in self.root
-
-
-linkml_meta = None
+from typing import Optional
+from pydantic import Field
+from datamodels import ConfiguredBaseModel
 
 
 class CTFMetadata(ConfiguredBaseModel):
