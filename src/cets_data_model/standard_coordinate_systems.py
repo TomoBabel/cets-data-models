96c698f2c6b18357b62d24cff71887f688f33bcc
Matthew Iadanza <matthew.iadanza@stfc.ac.uk>
2025-12-08 20:39:48 +0000

    Add helper functions and docs

---
+++ src/cets_data_model/standard_coordinate_systems.py
@@ -0,0 +1,43 @@
+from typing import Optional
+from cets_data_model.models.models import Axis, CoordinateSystem
+from cets_data_model.standard_names import (
+    BASE_LOGICAL_COORDS_2D,
+    BASE_LOGICAL_COORDS_3D,
+)
+
+# Axis definitions
+x_axis_logical = Axis(name="logical coordinates x axis", axis_unit="pixel/voxel")
+y_axis_logical = Axis(name="logical coordinates y axis", axis_unit="pixel/voxel")
+z_axis_logical = Axis(name="logical coordinates z axis", axis_unit="pixel/voxel")
+
+x_axis_physical = Axis(name="physical coordinates x axis", axis_unit="Ångstrom")
+y_axis_physical = Axis(name="physical coordinates y axis", axis_unit="Ångstrom")
+z_axis_physical = Axis(name="physical coordinates z axis", axis_unit="Ångstrom")
+
+# helper functions for generating coordinate systems
+
+
+def physical_coords(name: str, dim: int) -> CoordinateSystem:
+    """Generate physical coordinates object"""
+    axes = [x_axis_physical, y_axis_physical]
+    name = name
+    if dim == 3:
+        axes.append(z_axis_physical)
+    elif dim not in (2, 3):
+        raise ValueError(f"{dim} is not a valid dimension")
+    return CoordinateSystem(name=name, axes=axes)
+
+
+def logical_coords(name: Optional[str] = None, dim: int = 2) -> CoordinateSystem:
+    """Generate physical coordinates object
+
+    Gives the base logical coordinates if no name specified
+    """
+    name = BASE_LOGICAL_COORDS_2D if name is None else name
+    axes = [x_axis_logical, y_axis_logical]
+    if dim == 3:
+        name = BASE_LOGICAL_COORDS_3D
+        axes.append(z_axis_logical)
+    elif dim not in (2, 3):
+        raise ValueError(f"{dim} is not a valid dimension")
+    return CoordinateSystem(name=name, axes=axes)
--- src/cets_data_model/standard_transformations.py
+++ src/cets_data_model/standard_transformations.py
@@ -1,76 +1,33 @@
-from typing import Tuple, Optional
-from cets_data_model.models.models import Scale, CoordinateSystem, Axis
+from typing import Tuple
+from cets_data_model.models.models import (
+    Scale,
+    CoordinateSystem,
+    CoordinateTransformation,
+)
+from cets_data_model.standard_names import (
+    BASE_LOGICAL_COORDS_2D,
+    IMAGE_PIXEL_SIZE_COORDS,
+    IMAGE_PIXEL_SIZE_XFROM,
+    IMAGE_SUPER_RES_PIXEL_SIZE_XFROM,
+    IMAGE_SUPER_RES_PIXEL_SIZE_COORDS,
+    ALIGN_CALIBRATION_IMAGE_XFROM,
+    ALIGN_CALIBRATION_IMAGE_COORDS,
+    ALIGN_MAP_XFROM,
+    ALIGN_MAP_COORDS,
+    ALIGN_MOVIE_FRAME_XFROM,
+    ALIGN_MOVIE_FRAME_COORDS,
+    ALIGN_SUBTOMOGRAM_XFROM,
+    ALIGN_SUBTOMOGRAM_COORDS,
+    ALIGN_ANNOTATION_XFROM,
+    ALIGN_ANNOTATION_COORDS,
+)
+from cets_data_model.standard_coordinate_systems import logical_coords
 
-# Standard coordinate system and transformation names - The transformation or sequence
-# of transformations that accomplish these tasks must have these specific names and
-# the endpoint coordinate system must have the associated name.
+# helper functions for generating transformations and their associated coordinate
+# systems with the correct naming conventions. Each one returns the transformation
+# and the final coordinate system.
 
-# The actual transformation/sequence and coordinate system can vary as long as the
-# correct string from below is used in the `name` field.
-
-# Axis definitions
-x_axis_logical = Axis(name="logical coordinates x axis", axis_unit="pixel/voxel")
-y_axis_logical = Axis(name="logical coordinates y axis", axis_unit="pixel/voxel")
-z_axis_logical = Axis(name="logical coordinates z axis", axis_unit="pixel/voxel")
-
-x_axis_physical = Axis(name="physical coordinates x axis", axis_unit="Ångstrom")
-y_axis_physical = Axis(name="physical coordinates y axis", axis_unit="Ångstrom")
-z_axis_physical = Axis(name="physical coordinates z axis", axis_unit="Ångstrom")
-
-# NAMES - for the globals coordinate systems always end with COORDS, transformations
-# always end with XFORM
-
-# basic logical coords
-BASE_LOGICAL_COORDS = "base_logical_coordinates"
-
-# Align calibration image to movie frame
-ALIGN_CALIBRATION_IMAGE_XFROM = "align_calibration_image_to_movie_frame"
-ALIGN_CALIBRATION_IMAGE_COORDS = "aligned_calibration_image"
-
-# Align movie frame to projection
-ALIGN_MOVIE_FRAME_XFROM = "align_movie_frame_to_projection"
-ALIGN_MOVIE_FRAME_COORDS = "aligned_movie_frame"
-
-# Align projection image to tomogram
-ALIGN_PROJECTION_IMAGE_XFROM = "align_projection_image_to_tomogram"
-ALIGN_PROJECTION_IMAGE_COORDS = "aligned_projection_image"
-
-# Align subtomogram to tomogram R3D
-ALIGN_SUBTOMOGRAM_XFROM = "align_subtomogram_to_tomogram"
-ALIGN_SUBTOMOGRAM_COORDS = "aligned_subtomogram"
-
-# Align map to tomogram
-ALIGN_MAP_XFROM = "align_map_to_tomogram"
-ALIGN_MAP_COORDS = "aligned_map"
-
-# Align annotation to tomogram
-ALIGN_ANNOTATION_XFROM = "align_annotation_to_tomogram"
-ALIGN_ANNOTATION_COORDS = "aligned_annotation"
-
-# set pixel size of image
-IMAGE_PIXEL_SIZE_XFROM = "image_pixel_size"
-IMAGE_PIXEL_SIZE_COORDS = "image_pixel_size"
-
-# set super res pixel size of image
-IMAGE_SUPER_RES_PIXEL_SIZE_XFROM = "image_pixel_size"
-IMAGE_SUPER_RES_PIXEL_SIZE_COORDS = "image_pixel_size"
-
-# Helper functions
-
-
-def physical_coords(name: str) -> CoordinateSystem:
-    """Generate physical coordinates object"""
-    return CoordinateSystem(name=name, axes=[x_axis_physical, z_axis_physical])
-
-
-def logical_coords(name: Optional[str] = None) -> CoordinateSystem:
-    """Generate physical coordinates object
-
-    Gives the base logical coordinates if no name specified
-    """
-
-    name = BASE_LOGICAL_COORDS if name is None else name
-    return CoordinateSystem(name=name, axes=[x_axis_logical, z_axis_logical])
+# helper functions for scaling - just provide pixel size
 
 
 def image_pixel_size(apix: float) -> Tuple[Scale, CoordinateSystem]:
@@ -79,11 +36,11 @@ def image_pixel_size(apix: float) -> Tuple[Scale, CoordinateSystem]:
         Scale(
             type="scale",
             name=IMAGE_PIXEL_SIZE_XFROM,
-            input=BASE_LOGICAL_COORDS,
+            input=BASE_LOGICAL_COORDS_2D,
             output=IMAGE_PIXEL_SIZE_COORDS,
             scale=[apix, apix],
         ),
-        logical_coords(),
+        logical_coords(dim=2),
     )
 
 
@@ -94,9 +51,64 @@ def image_super_res_pixel_size(apix: float) -> Tuple[Scale, CoordinateSystem]:
         Scale(
             type="scale",
             name=IMAGE_SUPER_RES_PIXEL_SIZE_XFROM,
-            input=BASE_LOGICAL_COORDS,
+            input=BASE_LOGICAL_COORDS_2D,
             output=IMAGE_SUPER_RES_PIXEL_SIZE_COORDS,
             scale=[apix, apix],
         ),
-        logical_coords(),
+        logical_coords(dim=2),
     )
+
+
+# helper functions for transformations - provide the transformation(s). If more than
+# one transformation is used make sure to provide a Sequence transformation object.
+# Returns the transformation and the final coordinate system
+
+
+def align_calibration_image_to_movie_frame(
+    transformation: CoordinateTransformation,
+) -> Tuple[CoordinateTransformation, CoordinateSystem]:
+    transformation.name = ALIGN_CALIBRATION_IMAGE_XFROM
+    transformation.input = BASE_LOGICAL_COORDS_2D
+    transformation.output = ALIGN_CALIBRATION_IMAGE_COORDS
+    coords = logical_coords(ALIGN_CALIBRATION_IMAGE_COORDS)
+    return transformation, coords
+
+
+def align_movie_frame_to_projection(
+    transformation: CoordinateTransformation,
+) -> Tuple[CoordinateTransformation, CoordinateSystem]:
+    transformation.name = ALIGN_MOVIE_FRAME_XFROM
+    transformation.input = BASE_LOGICAL_COORDS_2D
+    transformation.output = ALIGN_MOVIE_FRAME_COORDS
+    coords = logical_coords(ALIGN_MOVIE_FRAME_COORDS)
+    return transformation, coords
+
+
+def align_subtomogram_to_tomogram(
+    transformation: CoordinateTransformation,
+) -> Tuple[CoordinateTransformation, CoordinateSystem]:
+    transformation.name = ALIGN_SUBTOMOGRAM_XFROM
+    transformation.input = BASE_LOGICAL_COORDS_2D
+    transformation.output = ALIGN_SUBTOMOGRAM_COORDS
+    coords = logical_coords(ALIGN_SUBTOMOGRAM_COORDS)
+    return transformation, coords
+
+
+def align_map_to_tomogram(
+    transformation: CoordinateTransformation,
+) -> Tuple[CoordinateTransformation, CoordinateSystem]:
+    transformation.name = ALIGN_MAP_XFROM
+    transformation.input = BASE_LOGICAL_COORDS_2D
+    transformation.output = ALIGN_MAP_COORDS
+    coords = logical_coords(ALIGN_MAP_COORDS)
+    return transformation, coords
+
+
+def align_annotation_to_tomogram(
+    transformation: CoordinateTransformation,
+) -> Tuple[CoordinateTransformation, CoordinateSystem]:
+    transformation.name = ALIGN_ANNOTATION_XFROM
+    transformation.input = BASE_LOGICAL_COORDS_2D
+    transformation.output = ALIGN_ANNOTATION_COORDS
+    coords = logical_coords(ALIGN_ANNOTATION_COORDS)
+    return transformation, coords
